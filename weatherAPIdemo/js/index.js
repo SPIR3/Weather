124752c50e9063783d7fa6ea7353a00a648682cb
SPIR3 <32450745+SPIR3@users.noreply.github.com>
2018-03-06 15:08:28 +0000

    Update index.js

--- weatherAPIdemo/js/index.js
+++ weatherAPIdemo/js/index.js
@@ -5,14 +5,19 @@ $(document).on('pagecreate', '#feedPage', function(event) {
 	
 	// Use an HTML GET request to obtain data from an API
 	var xmlhttp=new XMLHttpRequest();
-	xmlhttp.open("GET", feedURL, false);
+	xmlhttp.open("GET", feedURL, true);
 	xmlhttp.send();
 		
 		
 	// parse the resulting JSON into Javascript Data Object 
 	// you can use a live parser to inspect the contents of the JSON
 	// http://json.parser.online.fr/ 
-	var weather= JSON.parse(xmlhttp.responseText);
+	xmlhttp.onreadystatechange = function() {
+  	if (this.readyState == 4 && this.status == 200) {
+  		var weather= JSON.parse(xmlhttp.responseText);
+ 	 }
+	};
+	
 	
 	
 	//Define Ractive binding

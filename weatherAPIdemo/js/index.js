90f0891c4fd02f3ee41bfe0e7a88f73be1505d45
SPIR3 <32450745+SPIR3@users.noreply.github.com>
2018-03-06 15:21:45 +0000

    Update index.js

--- weatherAPIdemo/js/index.js
+++ weatherAPIdemo/js/index.js
@@ -5,27 +5,31 @@ $(document).on('pagecreate', '#feedPage', function(event) {
 	
 	// Use an HTML GET request to obtain data from an API
 	var xmlhttp=new XMLHttpRequest();
-	xmlhttp.open("GET", feedURL, true);
-	xmlhttp.send();
+	
 		
 		
 	// parse the resulting JSON into Javascript Data Object 
 	// you can use a live parser to inspect the contents of the JSON
 	// http://json.parser.online.fr/ 
 	xmlhttp.onreadystatechange = function() {
-  	if (this.readyState == 4 && this.status == 200) {
-  		var weather= JSON.parse(xmlhttp.responseText);
+  		if (this.readyState == 4 && this.status == 200) {
+  			var weather= JSON.parse(xmlhttp.responseText);
  	
 	
 	
 	
-	//Define Ractive binding
-	var ractive = new Ractive({
-    	el: 'container', <!-- where -->
-    	template: '#myTemplate', <!-- how -->
-    	data: { weather : weather.consolidated_weather } <!-- what - specify the list of weather reports using dot notation-->
-	});
+			//Define Ractive binding
+			var ractive = new Ractive({
+				el: 'container', <!-- where -->
+				template: '#myTemplate', <!-- how -->
+				data: { weather : weather.consolidated_weather } <!-- what - specify the list of weather reports using dot notation-->
+			});
 	
+		}
+	}
+	
+	
+	
+	xmlhttp.open("GET", feedURL, true);
+	xmlhttp.send();
 });
-		 }
-	};
